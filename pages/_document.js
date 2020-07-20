777c5b2d1a7b59698a310495e34b669f6111bc0a
Adam Kaewell <adam.kaewell@gmail.com>
2020-07-20 00:15:23 -0400

    fixed static folder to public folder due to deprecation, nextjs

--- pages/_document.js
+++ pages/_document.js
@@ -17,10 +17,10 @@ export default class MyDocument extends Document {
           <meta name="msapplication-starturl" content="/" />
           <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
-          <link rel="icon" type="image/png" sizes="512x512" href="/static/icons/icon-512x512.png" />
-          <link rel="apple-touch-icon" type="image/png" sizes="512x512" href="/static/icons/icon-512x512.png" />
-          <link rel="icon" type="image/png" sizes="192x192" href="/static/icons/icon-192x192.png" />
-          <link rel="apple-touch-icon" type="image/png" sizes="192x192" href="/static/icons/icon-192x192.png" />
+          <link rel="icon" type="image/png" sizes="512x512" href="icon-512x512.png" />
+          <link rel="apple-touch-icon" type="image/png" sizes="512x512" href="icon-512x512.png" />
+          <link rel="icon" type="image/png" sizes="192x192" href="icon-192x192.png" />
+          <link rel="apple-touch-icon" type="image/png" sizes="192x192" href="icon-192x192.png" />
         </Head>
         <body>
           <Main />
